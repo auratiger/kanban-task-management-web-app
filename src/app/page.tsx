75cbc681b6a78321cea7715b54c7174c2826c4d3
Georgi Georgiev <auratiger00@gmail.com>
2023-04-10 11:49:52 +0300

    custom scrollbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import { boards } from "sampleData/data";
 
-import Button from "@/components/Button";
 import Board from "@/components/Board";
+import Button from "@/components/Button";
 
 export default function Home() {
   const board = boards[0];
   const isBoardEmpty: boolean = board.columns.length <= 0;
 
   return (
-    <main className="flex flex-1 overflow-auto bg-grey-light p-8 dark:bg-grey-vdark">
+    <main className="scrollbar mb-1 flex flex-1 overflow-auto bg-grey-light p-8 dark:bg-grey-vdark">
       {isBoardEmpty && (
         <div className="flex flex-1 flex-col items-center gap-4 self-center justify-self-center">
           <span>This board is empty. Create a new column to get started.</span>
